d2acc0f4933236faeca0a1a718bf29cb1c3dc4b1
Dr. Mike Hopper <drmikeh@gmail.com>
2016-08-28 17:46:49 -0400

    Added service to pets-app.

--- pets-app/app.js
+++ pets-app/app.js
@@ -14,15 +14,21 @@ angular.module('petsApp')
     </article>
     <button ng-click="$ctrl.removeLastPet()">Remove Last Pet</button>
 `,
-  controller: function petsListCtrl() {
-    this.pets = [
+  controller: function petsListCtrl(petService) {
+    this.pets = petService.pets;
+    this.removeLastPet = petService.removeLastPet;
+  }
+});
+
+angular.module('petsApp')
+.service('petService', function() {
+  this.pets = [
       { name: 'Meisha', species: 'dog', owner: 'Mike'   , vaccinated: true },
       { name: 'Deisel', species: 'dog', owner: 'Marc'   , vaccinated: false },
       { name: 'Snoopy', species: 'dog', owner: 'Charlie', vaccinated: true },
       { name: 'Felix' , species: 'cat', owner: 'Susan'  , vaccinated: true }
     ];
-    this.removeLastPet = function() {
-      this.pets.pop();
-    };
-  }
+  this.removeLastPet = function() {
+    this.pets.pop();
+  };
 });
